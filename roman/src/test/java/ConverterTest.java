1bc9a713d2aafcd261f623accf9f83e2294e7212
Erik Tittel <et23@live.de>
2015-09-29 22:09:51 +0200

    Final Version for roman converter code kata

--- roman/src/test/java/ConverterTest.java
+++ roman/src/test/java/ConverterTest.java
@@ -1,15 +1,31 @@
 import org.junit.Test;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 /**
+ * Test for converting arabic numbers in roman numbers.
+ *
  * Created by Erik on 26.09.2015.
  */
 public class ConverterTest {
 
+    private Map<Integer, String> romanSymbols = new TreeMap<>((Integer a, Integer b) ->  b - a);
+    {
+        romanSymbols.put(1, "I");
+        romanSymbols.put(4, "IV");
+        romanSymbols.put(5, "V");
+        romanSymbols.put(10, "X");
+    }
+
     private String convert(int arabic) {
-        if (arabic == 1) return "I";
+        for (Integer value : romanSymbols.keySet()) {
+            if (arabic >= value)
+                return romanSymbols.get(value) + convert(arabic - value);
+        }
         return "";
     }
 
@@ -17,5 +33,13 @@ public class ConverterTest {
     public void testConvert() {
         assertThat(convert(0), is(""));
         assertThat(convert(1), is("I"));
+        assertThat(convert(2), is("II"));
+        assertThat(convert(3), is("III"));
+        assertThat(convert(4), is("IV"));
+        assertThat(convert(5), is("V"));
+        assertThat(convert(6), is("VI"));
+        assertThat(convert(7), is("VII"));
+        assertThat(convert(8), is("VIII"));
+        assertThat(convert(10), is("X"));
     }
 }
